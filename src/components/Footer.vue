1af4abb788ecd64ec2dbe141a9db103cca9d809d
KoofNG <isrealakpan296@gmail.com>
2019-05-17 22:35:16 +0100

    Completed the Footer Component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
+    <div id="footer">
         
+        <hr/>
+        <p>&copy; 2019 - Made with <span id='coloredHeart'>&#10084;</span> by <a href="https://github.com/koofng">KoofNG</a></p>
     </div>
 </template>
 
@@ -11,5 +13,39 @@ export default {
 </script>
 
 <style scoped>
+/* Block */
+#footer {
+    /* background-color: #5f5e5e; */
+    background-color: #3b4045;
+    padding: 30px 40px;
+    margin: 0px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+}
+
+/* Element */
+
+hr {
+    width: 100%;
+    position: relative;
+    display: block;
+    border: .5px solid #afacac;
+    margin: 0px;
+    padding: 0px;
+}
+p {
+    color: #fffeee;
+    font-weight: lighter;
+}
+p a {
+    text-decoration: none;
+    color: inherit;
+}
+/* Modifier */
+#coloredHeart {
+    color: #ff7437;
+}
     
 </style>
