6452b86c044540949fcd131f58be42812baae592
KoofNG <isrealakpan296@gmail.com>
2019-05-17 21:06:35 +0100

    Added Header and footer component for all user page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
     <div>
-        
+        <Header/>
+        <Footer/>        
     </div>
 </template>
 
 <script>
+
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+
 export default {
-    
+    components: {
+        Header,
+        Footer
+    }
 }
 </script>
 
 <style scoped>
 
 </style>
-
