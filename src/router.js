280536a840de99e48a798c84cd1fd71f5ce0aee1
KoofNG <isrealakpan296@gmail.com>
2019-05-17 21:00:02 +0100

    Added the routes path for the registration of users

--- src/router.js
+++ src/router.js
@@ -1,6 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+
+
+import Home from './views/Home';
+import Form from './views/Register';
+import Preview from './views/Preview';
+import Success from './views/Success';
 
 Vue.use(Router)
 
@@ -12,12 +17,19 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/register',
+      name: 'register',
+      component: Form
+    },
+    {
+      path: '/preview',
+      name: 'register',
+      component: Preview
+    },
+    {
+      path: '/success',
+      name: 'register',
+      component: Success
     }
   ]
 })
