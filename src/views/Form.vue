3fb57c14d52d5b6089cafe8c2cf5edbcf1c0a27b
KoofNG <isrealakpan296@gmail.com>
2019-05-18 18:40:41 +0100

    Updated the Register View
    Imported the Form Component for use in the register view

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <Header/>
+        <Forms/>
         <Footer/> 
     </div>
 </template>
@@ -8,16 +9,25 @@
 <script>
 
 import Header from '@/components/Header.vue';
+import Forms from '@/components/Forms.vue';
 import Footer from '@/components/Footer.vue';
+
 
 export default {
     components: {
         Header,
+        Forms,
         Footer
     }
 }
 </script>
 
 <style scoped>
+
+@media screen and (max-width: 767px) {
+    div#footer {
+        margin-top: 0px;
+    }
+}
 
 </style>
