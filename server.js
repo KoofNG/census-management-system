685d9282a924177ccd5ca584cf4987442cccdfe0
KoofNG <isrealakpan296@gmail.com>
2019-05-22 05:39:57 +0100

    Updated server port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'dist')));
 
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 3000,()=>console.log("server running"));
 
 // console.log('Server listening on port 3000');
